0b841569af34a6059e52a05077b2668e2088c813
GabrieleLopes0 <gabrielelopesbibi@gmail.com>
2026-04-13 21:53:19 -0300

    feat: criando modelo dos cards a serem utilizados

---
+++ backend/src/main/java/com/kanban/kanban/model/Card.java
@@ -0,0 +1,78 @@
+package com.kanban.kanban.model;
+
+import java.time.LocalDateTime;
+
+public class Card {
+
+    private Long id;
+
+    private String titulo;
+
+    private String descricao;
+
+    private StatusCard status;
+
+    private Integer prioridade;
+
+    private LocalDateTime dataCriacao;
+
+    public Card() {
+    }
+
+    public Card(Long id, String titulo, String descricao, StatusCard status, Integer prioridade, LocalDateTime dataCriacao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+        this.prioridade = prioridade;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusCard getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusCard status) {
+        this.status = status;
+    }
+
+    public Integer getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Integer prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
