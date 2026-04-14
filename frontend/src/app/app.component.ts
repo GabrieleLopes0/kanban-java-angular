628e9d2257af462b64eae95a36802b538dc6aaa7
GabrieleLopes0 <gabrielelopesbibi@gmail.com>
2026-04-13 22:43:13 -0300

    feat: adicionado pequeno formulário de cadastro para os cards

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 import { CardService } from './services/card.service'
 import { Card } from './models/card'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
   todo: Card[] = []
   doing: Card[] = []
   done: Card[] = []
+
+  novoTitulo = ''
+  novaDescricao = ''
+  novaPrioridade = 1
 
   constructor(private service: CardService) {}
 
@@ -32,7 +37,21 @@ export class AppComponent implements OnInit {
 
   mover(card: Card, status: string) {
     this.service.updateStatus(card.id, status)
+      .subscribe(() => this.carregarCards())
+  }
+
+  criar() {
+    const card = {
+      titulo: this.novoTitulo,
+      descricao: this.novaDescricao,
+      prioridade: this.novaPrioridade
+    }
+
+    this.service.createCard(card)
       .subscribe(() => {
+        this.novoTitulo = ''
+        this.novaDescricao = ''
+        this.novaPrioridade = 1
         this.carregarCards()
       })
   }
